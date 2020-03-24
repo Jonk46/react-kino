bff10201bd9bd70474096298699ddc58ad9f6c92
Alex <jonk6233@gmail.com>
2020-03-25 00:12:00 +0200

    delete films

---
+++ src/components/MovieItem.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const MovieItem = props => {
+  console.log(props);
+  return (
+    <div>
+      <p>{props.movie.title}</p>
+      <button onClick={props.removeMovie.bind(null, props.movie)}>
+        Delete Movie
+  </button>
+    </div>
+  );
+};
+
+
+
+export default MovieItem;
