404b4a931aef32397cf144716ba1c479c77971e4
Alex <jonk6233@gmail.com>
2020-04-10 21:44:00 +0300

    Updates

---
+++ precache-manifest.50755075e46ad1b9611e6072109b06ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f904200cc81123991cc631459751e54e",
+    "url": "/react-kino/index.html"
+  },
+  {
+    "revision": "7b178eb1d8c007b4d346",
+    "url": "/react-kino/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "5aca03e0101bb827132f",
+    "url": "/react-kino/static/css/main.5d937b94.chunk.css"
+  },
+  {
+    "revision": "7b178eb1d8c007b4d346",
+    "url": "/react-kino/static/js/2.d9cdd051.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-kino/static/js/2.d9cdd051.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5aca03e0101bb827132f",
+    "url": "/react-kino/static/js/main.b08ee081.chunk.js"
+  },
+  {
+    "revision": "f757be89bf072bfe8968",
+    "url": "/react-kino/static/js/runtime-main.72cb2b84.js"
+  }
+]);
